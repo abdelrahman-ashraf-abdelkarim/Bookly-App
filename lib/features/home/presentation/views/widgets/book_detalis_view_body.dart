288871aaf2a645a6536f80ad1388d8db0ac96a2c
Abdelrahman Ashraf <8effort@gmail.com>
2026-07-18 00:07:41 +0300

    add custom book image to book details view

--- lib/features/home/presentation/views/widgets/book_detalis_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_detalis_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_app_bar.dart';
@@ -7,10 +8,19 @@ class BookDetalisViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [const CustomBookDetailsAppBar()]),
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBar(),
+          const SizedBox(height: 40),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.19),
+            child: const CustomBookImage(),
+          ),
+        ],
+      ),
     );
   }
 }
-
