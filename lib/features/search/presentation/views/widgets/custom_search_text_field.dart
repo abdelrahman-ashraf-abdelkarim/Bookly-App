2a236c72b4e9478847fc150e8f05e1b8780bdd89
Abdelrahman Ashraf <8effort@gmail.com>
2026-07-18 21:34:30 +0300

    search view ui done

--- lib/features/search/presentation/views/widgets/custom_search_text_field.dart
+++ lib/features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -6,18 +6,21 @@ class CustomSearchTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Search',
-        suffixIcon: IconButton(
-          onPressed: () {},
-          icon: const Opacity(
-            opacity: 0.8,
-            child: FaIcon(FontAwesomeIcons.magnifyingGlass, size: 22),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search',
+          suffixIcon: IconButton(
+            onPressed: () {},
+            icon: const Opacity(
+              opacity: 0.8,
+              child: FaIcon(FontAwesomeIcons.magnifyingGlass, size: 22),
+            ),
           ),
+          enabledBorder: buildOutlineInputBorder(),
+          focusedBorder: buildOutlineInputBorder(),
         ),
-        enabledBorder: buildOutlineInputBorder(),
-        focusedBorder: buildOutlineInputBorder(),
       ),
     );
   }
